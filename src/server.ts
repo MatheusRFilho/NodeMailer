145e77bf27c2cef7ec137ecdab5884e64685e392
Matheus Filho <matheusrodrigofilho@outlook.com>
2021-02-22 15:24:21 -0300

    Criando Rota get e rota POST

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,23 @@ import express from 'express';
 
 const app = express();
 
+/**
+ * Metódos HTTP
+ * 
+ * get      => Buscar
+ * post     => Salvar
+ * put      => Alterar
+ * delete   => Deletar
+ * patch    => Alteração especifica
+ * 
+*/
+
+app.get('/', (request, response) => {
+    return response.json({message: 'test'});
+});
+
+app.post('/', (request, response) => {
+    return response.json({message: 'Os dados foram salvos com sucesso'})
+});
+
 app.listen(3333, () => console.log("Server is Running!"));
