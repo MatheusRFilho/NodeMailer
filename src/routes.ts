b1846c9239084209a435e2a8b95bdfd10012c702
Matheus Filho <matheusrodrigofilho@outlook.com>
2021-02-24 16:40:22 -0300

    Criação da rota controller e repository

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { UserController } from "./controllers/UserController";
+import { SurveysController } from "./controllers/SurveysController";
 
 import { Router } from 'express';
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
+
 
 router.post('/users', userController.create);
+router.post('/surveys', surveysController.create);
 
 export { router }
 
