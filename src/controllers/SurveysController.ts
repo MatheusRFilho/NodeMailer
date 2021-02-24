f7a43dccfeb9197f078ee8c6f9ca5716c54cf463
Matheus Filho <matheusrodrigofilho@outlook.com>
2021-02-24 16:49:38 -0300

    Criando uma listagem de todas as surveys

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -16,6 +16,16 @@ class SurveysController {
 
         return response.status(201).json(survey);
     }
+    
+    async show( request: Request, response: Response ) {
+
+        const surveysRepository = getCustomRepository(SurveysRepository);
+
+        const surveys = await surveysRepository.find();
+
+
+        return response.status(200).json(surveys);
+    }
 }  
 
 export { SurveysController }
