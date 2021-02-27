7edd73544fa672be25dd08a67ad7f7c707d10204
Matheus Filho <matheusrodrigofilho@outlook.com>
2021-02-27 03:06:28 -0300

    Controller de Calculo de nps

--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,14 @@ import { SurveysController } from "./controllers/SurveysController";
 import { Router } from 'express';
 import { SendMailController } from "./controllers/SendMailController";
 import { AnswerController } from "./controllers/AnswerController";
+import { NpsController } from "./controllers/NpsController";
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
 
 router.post('/users', userController.create);
@@ -19,7 +21,9 @@ router.get('/surveys', surveysController.show);
 
 router.post('/sendMail', sendMailController.execute);
 
-router.get('/answers/:value', answerController.execute)
+router.get('/answers/:value', answerController.execute);
+
+router.get('/nps/:survey_id', npsController.execute)
 
 export { router }
 
